04f30b0347bca7a88b11dc7eb7d873bdd91ad228
Igor Oliveira Pereira <igorp9971@gmail.com>
2019-09-24 14:18:09 -0300

    Remoção de forms, correção Assembly

--- dicionario/Properties/AssemblyInfo.cs
+++ dicionario/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associada a um assembly.
-[assembly: AssemblyTitle("dicionario")]
+[assembly: AssemblyTitle("Dicionario Tradlex")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // utilizando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.3.11.11")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
